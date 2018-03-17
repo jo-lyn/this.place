ae01ef2a88bbe662bddfe14338bcdcbbf3b09705
jo-lyn <e0007247@u.nus.edu>
2018-03-17 16:22:08 +0800

    chore: update mapping for face highlight

--- Assets/Scripts/UVMap.cs
+++ Assets/Scripts/UVMap.cs
@@ -37,7 +37,7 @@ public class UVMap : MonoBehaviour, ITransparentRenderer
 
     private void SetUVs()
     {
-        Vector2[] blockUVs = new Vector2[24];
+        Vector2[] blockUVs = (Vector2[])_mesh.uv.Clone();
 
         // Top
         blockUVs[4] = new Vector2(0.0f, 0.75f);
@@ -116,7 +116,7 @@ public class UVMap : MonoBehaviour, ITransparentRenderer
         blockUVs[23] = new Vector2(umax, vmin);
 
         _mesh.uv = blockUVs;
-    }*/
+    }
 
     public void SetFaceHighlight(BlockFace face)
     {
@@ -168,6 +168,58 @@ public class UVMap : MonoBehaviour, ITransparentRenderer
                 blockUVs[17] = maxmin;
                 blockUVs[18] = maxmax;
                 blockUVs[19] = minmin;
+                break;
+        }
+
+        _mesh.uv = blockUVs;
+    }*/
+
+    public void SetFaceHighlight(BlockFace face)
+    {
+        Vector2[] blockUVs = (Vector2[])_mesh.uv.Clone();
+
+        switch (face)
+        {
+            case BlockFace.Top:
+                blockUVs[4] = new Vector2(0.0f, 0.25f);
+                blockUVs[5] = new Vector2(0.25f, 0.25f);
+                blockUVs[8] = new Vector2(0.0f, 0.5f);
+                blockUVs[9] = new Vector2(0.25f, 0.5f);
+                break;
+
+            case BlockFace.Bottom:
+                blockUVs[15] = new Vector2(0.0f, 0.0f);
+                blockUVs[12] = new Vector2(0.25f, 0.0f);
+                blockUVs[14] = new Vector2(0.0f, 0.25f);
+                blockUVs[13] = new Vector2(0.25f, 0.25f);
+                break;
+
+            case BlockFace.North:
+                blockUVs[0] = new Vector2(0.25f, 0.25f);
+                blockUVs[1] = new Vector2(0.5f, 0.25f);
+                blockUVs[2] = new Vector2(0.25f, 0.5f);
+                blockUVs[3] = new Vector2(0.5f, 0.5f);
+                break;
+
+            case BlockFace.South:
+                blockUVs[6] = new Vector2(0.5f, 0.25f);
+                blockUVs[7] = new Vector2(0.75f, 0.25f);
+                blockUVs[10] = new Vector2(0.5f, 0.5f);
+                blockUVs[11] = new Vector2(0.75f, 0.5f);
+                break;
+
+            case BlockFace.East:
+                blockUVs[23] = new Vector2(0.25f, 0.0f);
+                blockUVs[20] = new Vector2(0.5f, 0.0f);
+                blockUVs[22] = new Vector2(0.25f, 0.25f);
+                blockUVs[21] = new Vector2(0.5f, 0.25f);
+                break;
+
+            case BlockFace.West:
+                blockUVs[19] = new Vector2(0.5f, 0.0f);
+                blockUVs[16] = new Vector2(0.75f, 0.0f);
+                blockUVs[18] = new Vector2(0.5f, 0.25f);
+                blockUVs[17] = new Vector2(0.75f, 0.25f);
                 break;
         }
 
