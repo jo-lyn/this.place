2dddb2f9f503fe4314a8631134fb9f183b4db535
jo-lyn <e0007247@u.nus.edu>
2018-04-14 18:53:47 +0800

    feat: add float up on trigger

--- Assets/Scripts/CollectiblesScript.cs
+++ Assets/Scripts/CollectiblesScript.cs
@@ -7,11 +7,13 @@ public class CollectiblesScript : MonoBehaviour
     public ParticleSystem CollectibleEmitter;
     public int OptionalNumber = 0;
     private float _rotateSpeed = 0.3f;
+    private float _floatSpeed = 0;
     private ParticleSystem _collectibleEmitterInstantiated;
 
     void Update()
     {
         transform.RotateAround(transform.position, Vector3.up, _rotateSpeed);
+        transform.position += new Vector3(0, _floatSpeed, 0);
     }
 
     private void OnTriggerEnter(Collider colider)
@@ -20,10 +22,11 @@ public class CollectiblesScript : MonoBehaviour
         {
             SendMessageUpwards("UpdateScore", this);
             _rotateSpeed += 10f;
+            _floatSpeed = 0.02f;
             Destroy(gameObject, 1f);
 
             _collectibleEmitterInstantiated = Instantiate(CollectibleEmitter, transform.position, Quaternion.identity) as ParticleSystem;
-            //_collectibleEmitterInstantiated.GetComponent<ParticleSystemRenderer>().material = GetComponent<Renderer>().material;
+            _collectibleEmitterInstantiated.GetComponent<ParticleSystemRenderer>().material.color = GetComponent<Renderer>().material.color;
             _collectibleEmitterInstantiated.Play();
             Destroy(_collectibleEmitterInstantiated, 1.5f);
         }
